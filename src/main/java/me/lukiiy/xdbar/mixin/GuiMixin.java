85e6319a44616e9c684eac85b235e8e08976485c
Lukiiy <102190774+Lukiiy@users.noreply.github.com>
2026-06-20 02:59:57 -0300

    26.2's refactor backported to 26.1

--- src/main/java/me/lukiiy/xdbar/mixin/GuiMixin.java
+++ src/main/java/me/lukiiy/xdbar/mixin/GuiMixin.java
@@ -6,19 +6,16 @@ import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.DeltaTracker;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.Font;
 import net.minecraft.client.gui.Gui;
 import net.minecraft.client.gui.GuiGraphicsExtractor;
 import net.minecraft.client.gui.contextualbar.ContextualBarRenderer;
 import net.minecraft.client.gui.contextualbar.LocatorBarRenderer;
-import net.minecraft.network.chat.Component;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
@@ -32,7 +29,6 @@ public class GuiMixin {
     @Shadow @Final private Map<?, Supplier<ContextualBarRenderer>> contextualInfoBarRenderers;
 
     @Unique private LocatorBarRenderer locatorRenderer;
-    @Unique private static final int TEXT_OUTLINE = -16777216;
 
     @Inject(method = "<init>", at = @At("TAIL"))
     private void xdBar$getRenderer(Minecraft minecraft, CallbackInfo ci) {
@@ -41,20 +37,6 @@ public class GuiMixin {
                 .map(LocatorBarRenderer.class::cast)
                 .findFirst()
                 .ifPresent(render -> locatorRenderer = render);
-    }
-
-    @Redirect(method = "extractHotbarAndDecorations", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/contextualbar/ContextualBarRenderer;extractExperienceLevel(Lnet/minecraft/client/gui/GuiGraphicsExtractor;Lnet/minecraft/client/gui/Font;I)V"))
-    private void xdBar$displayLevel(GuiGraphicsExtractor graphics, Font font, int experienceLevel) {
-        if (!XDBar.shadow && XDBar.outline && XDBar.color == XDBar.DEF_COLOR && XDBar.offsetY == XDBar.DEF_OFFSET) ContextualBarRenderer.extractExperienceLevel(graphics, font, experienceLevel);
-        else {
-            Component value = Component.translatable("gui.experience.level", experienceLevel);
-            int x = (graphics.guiWidth() - font.width(value)) / 2;
-            int y = graphics.guiHeight() - XDBar.offsetY;
-
-            if (XDBar.outline) XDBar.textOutline(graphics, font, value, x, y, TEXT_OUTLINE);
-
-            graphics.text(font, value, x, y, XDBar.color, XDBar.shadow);
-        }
     }
 
     @Inject(method = "extractHotbarAndDecorations", at = @At("TAIL"))
