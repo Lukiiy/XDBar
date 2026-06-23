1045e3943c542a19b21540350e30326ebd1369e9
Lukiiy <102190774+Lukiiy@users.noreply.github.com>
2026-06-23 13:23:11 -0300

    Optimize Imports

--- src/main/java/me/lukiiy/xdbar/ConfigMenu.java
+++ src/main/java/me/lukiiy/xdbar/ConfigMenu.java
@@ -17,7 +17,6 @@ import net.minecraft.client.gui.narration.NarrationElementOutput;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.CommonComponents;
 import net.minecraft.network.chat.Component;
-import net.minecraft.util.ARGB;
 
 import java.util.Arrays;
 import java.util.List;
