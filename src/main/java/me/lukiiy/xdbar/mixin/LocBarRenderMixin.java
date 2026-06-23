69be43191f0cd6a6cae4d42f7c401853ed016b90
Lukiiy <102190774+Lukiiy@users.noreply.github.com>
2026-06-23 12:44:03 -0300

    Some LocBarRenderMixin
    * Readded outlines
    * Waypoint Decorations now only work if the waypoint is not above/below the camera
    * Rework pitch arrow toggle to be less aggressive

--- src/main/java/me/lukiiy/xdbar/mixin/LocBarRenderMixin.java
+++ src/main/java/me/lukiiy/xdbar/mixin/LocBarRenderMixin.java
@@ -9,17 +9,18 @@ import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.contextualbar.LocatorBarRenderer;
 import net.minecraft.client.renderer.RenderPipelines;
 import net.minecraft.client.resources.WaypointStyle;
-import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.Mth;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.waypoints.TrackedWaypoint;
 import net.minecraft.world.waypoints.Waypoint;
+import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
@@ -38,9 +39,9 @@ public abstract class LocBarRenderMixin {
         if (!XDBar.pins) ci.cancel();
     }
 
-    @Inject(method = "method_70870", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/GuiGraphics;blitSprite(Lcom/mojang/blaze3d/pipeline/RenderPipeline;Lnet/minecraft/resources/ResourceLocation;IIII)V"), cancellable = true)
-    private void xdBar$arrows(Level level, GuiGraphics guiGraphics, int i, TrackedWaypoint trackedWaypoint, CallbackInfo ci) {
-        if (!XDBar.arrows) ci.cancel();
+    @ModifyVariable(method = "method_70870", at = @At(value = "STORE"), ordinal = 0)
+    private TrackedWaypoint.PitchDirection xdBar$arrows(TrackedWaypoint.PitchDirection original) {
+        return XDBar.arrows ? original : TrackedWaypoint.PitchDirection.NONE;
     }
 
     @Inject(method = "method_70870", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/GuiGraphics;blitSprite(Lcom/mojang/blaze3d/pipeline/RenderPipeline;Lnet/minecraft/resources/ResourceLocation;IIII)V", shift = At.Shift.AFTER), locals = LocalCapture.CAPTURE_FAILSOFT)
@@ -50,16 +51,23 @@ public abstract class LocBarRenderMixin {
         guiGraphics.blitSprite(RenderPipelines.GUI_TEXTURED, resourceLocation2, j + l + 1, i + m, 7, 5, k);
     }
 
-    @Inject(method = "method_70870", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/GuiGraphics;blitSprite(Lcom/mojang/blaze3d/pipeline/RenderPipeline;Lnet/minecraft/resources/ResourceLocation;IIIII)V", shift = At.Shift.AFTER), locals = LocalCapture.CAPTURE_FAILSOFT)
-    private void xdBar$deco(Level level, GuiGraphics guiGraphics, int i, TrackedWaypoint trackedWaypoint, CallbackInfo ci, double d, int j, Waypoint.Icon icon, WaypointStyle waypointStyle, float f, ResourceLocation resourceLocation, int k, int l) {
-        int distOffset = 15;
-        if (!XDBar.distanceDisplay || l < -distOffset || l > distOffset || waypointStyle.spriteLocations().size() == 1 || resourceLocation.equals(waypointStyle.spriteLocations().getLast())) return;
+    @Inject(method = "method_70870", at = @At(value = "FIELD", target = "Lnet/minecraft/world/waypoints/TrackedWaypoint$PitchDirection;NONE:Lnet/minecraft/world/waypoints/TrackedWaypoint$PitchDirection;", shift = At.Shift.AFTER, opcode = Opcodes.GETSTATIC), locals = LocalCapture.CAPTURE_FAILSOFT)
+    private void xdBar$deco(Level level, GuiGraphics guiGraphics, int i, TrackedWaypoint trackedWaypoint, CallbackInfo ci, double d, int j, Waypoint.Icon icon, WaypointStyle waypointStyle, float f, ResourceLocation resourceLocation, int k, int l, TrackedWaypoint.PitchDirection pitchDirection) {
+        int distOffset = 30;
+
+        if (!XDBar.distanceDisplay || l < -distOffset || l > distOffset || waypointStyle.spriteLocations().size() == 1 || resourceLocation.equals(waypointStyle.spriteLocations().getLast()) || pitchDirection != TrackedWaypoint.PitchDirection.NONE) return;
 
         String text = Mth.floor(f) + "";
         int x = j + l + 4 - minecraft.font.width(text) / 2 + 1;
         int y = i - 2;
 
-        XDBar.textOutline(guiGraphics, minecraft.font, Component.literal(text), x, y, 0xFF000000);
+        // outline
+        guiGraphics.drawString(minecraft.font, text, x + 1, y, XDBar.NEUTRAL, false);
+        guiGraphics.drawString(minecraft.font, text, x - 1, y, XDBar.NEUTRAL, false);
+        guiGraphics.drawString(minecraft.font, text, x, y + 1, XDBar.NEUTRAL, false);
+        guiGraphics.drawString(minecraft.font, text, x, y - 1, XDBar.NEUTRAL, false);
+
+        // distance
         guiGraphics.drawString(minecraft.font, text, x, y, k, false);
     }
 }
